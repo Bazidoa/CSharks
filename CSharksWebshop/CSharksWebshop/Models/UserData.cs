be37254aa1e0feaa594e31cac5a15b1dee8cd3a2
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-05-15 16:14:30 +0200

    UserData.cs created, prop + ctor done

---
+++ CSharksWebshop/CSharksWebshop/Models/UserData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharksWebshop.Models
+{
+    public class UserData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public List<Address> UserAddresses { get; set; }
+
+        public UserData(string fname, string lname, string uname, Address address)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.UserName = uname;
+            this.UserAddresses.Add(address);
+        }
+
+        public List<Order> ShowMyOrders()
+        {
+            //ahhoz ezt meg lehessen csinálni valami keresztül össze kell kapcsolni a userrel majd az orderekkel
+            //userID adattag ide?
+        }
+    }
+}
