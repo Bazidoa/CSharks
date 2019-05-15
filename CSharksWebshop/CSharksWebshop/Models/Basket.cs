ae0ce15f0b35a8c38a6c5861d4386c667453cb72
Rómeó Fekete <feketelaszloromeo@gmail.com>
2019-05-15 15:21:21 +0200

    Basket

---
+++ CSharksWebshop/CSharksWebshop/Models/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharksWebshop.Models
+{
+    public class Basket : ItemContainer
+    {
+        public void AddProduct()
+        {
+
+        }
+    }
+}
