ad5a1685956155407802c03f3059927f5c5dbc03
Antoni Péter <mrpantoni@gmail.com>
2019-05-15 14:30:39 +0200

    Address and ItemContainer class added

---
+++ CSharksWebshop/CSharksWebshop/Models/ItemContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharksWebshop.Models
+{
+    public abstract class ItemContainer
+    {
+        private string userId;
+        private List<Product> products;
+
+        public List<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        public string UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        public void RemoveContainerItem()
+        {
+
+        }
+        public void EmptyContainer()
+        {
+
+        }
+        public int SumItemsPrice()
+        {
+
+        }
+    }
+}
