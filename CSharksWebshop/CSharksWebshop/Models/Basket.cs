19c109e7f8c7f08ca7e5ad8ca9f17c6a05c85a8b
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-05-15 16:03:07 +0200

    Basket.cs done

--- CSharksWebshop/CSharksWebshop/Models/Basket.cs
+++ CSharksWebshop/CSharksWebshop/Models/Basket.cs
@@ -7,9 +7,37 @@ namespace CSharksWebshop.Models
 {
     public class Basket : ItemContainer
     {
-        public void AddProduct()
+
+        public List<Product> basketProducts = new List<Product>();
+        public string UserID { get; set; }
+
+
+        public void AddProduct(Product productToAdd)
+        {
+            basketProducts.Add(productToAdd);
+        }
+
+        //Ezeket a metódusokat örökli az ItemContainer abstract osztályból
+       /* 
+        public void RemoveProduct(Product productToRemove)
         {
 
         }
+
+        public void EmptyBasket()
+        {
+            this.basketProducts = new List<Product>();
+        }
+
+        public int SumBasketPrice()
+        {
+            int sum = 0;
+            foreach (Product product in basketProducts)
+            {
+                sum += product.ProductPrice;
+            }
+            return sum;
+        }
+        */
     }
 }
