b2614a4bb4a6fe0bafa062d825b6aeb7646d9b74
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-05-16 18:20:25 +0200

    SessionBasketEntry created

---
+++ CSharksWebshop/CSharksWebshop/Models/SessionBasketEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharksWebshop.Models
+{
+    public class SessionBasketEntry
+    {
+        public string ID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
