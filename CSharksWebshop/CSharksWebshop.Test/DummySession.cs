9f636370935b9e29c7a7d74f3737997c62d2d1dd
Antoni Péter <mrpantoni@gmail.com>
2019-05-22 13:56:59 +0200

    Test: UserAuthentication updated

---
+++ CSharksWebshop/CSharksWebshop.Test/DummySession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.SessionState;
+
+namespace CSharksWebshop.Test
+{
+    class DummySession: HttpSessionStateBase
+    {
+        private string mySessionID = null;
+        public DummySession(string mySessionID)
+        {
+            this.mySessionID = mySessionID;
+        }
+        public override string SessionID
+        {
+            get
+            {
+                return this.mySessionID;
+            }
+        }
+    }
+}
